27decb0cf2349f6b7d81e80802cc53b26fbec651
Bert Vereecken <bert.vereecken@ptvlogistics.com>
2026-05-13 09:56:27 +0200

    delete impl and tests for repo layer

--- src/repo/todoRepo.ts
+++ src/repo/todoRepo.ts
@@ -8,31 +8,15 @@ export interface Todo {
 let todos: Todo[] = [];
 
 export const todoRepo = {
-  getAll: (): Todo[] => [...todos],
+  getAll: () => {},
 
-  getById: (id: string): Todo | undefined => todos.find((t) => t.id === id),
+  getById: (id: string): Todo | undefined => {},
 
-  create: (title: string): Todo => {
-    const todo: Todo = {
-      id: crypto.randomUUID(),
-      title,
-      completed: false,
-      createdAt: Date.now(),
-    };
-    todos.push(todo);
-    return todo;
-  },
+  create: (title: string): Todo => {},
 
-  update: (id: string, data: Partial<Pick<Todo, "title" | "completed">>): Todo => {
-    const idx = todos.findIndex((t) => t.id === id);
-    if (idx === -1) throw new Error("Todo not found");
-    todos[idx] = { ...todos[idx], ...data };
-    return todos[idx];
-  },
+  update: (id: string, data: Partial<Pick<Todo, "title" | "completed">>): Todo => {},
 
-  delete: (id: string): void => {
-    todos = todos.filter((t) => t.id !== id);
-  },
+  delete: (id: string): void => {},
 
   /** @internal test only */
   _reset: (): void => {
